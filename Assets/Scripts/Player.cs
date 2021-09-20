aeb9946e8f146ca6fec3156465f7eb22e3fc02f3
Muhammed ARSLAN <muhammedg3@gmail.com>
2021-09-20 23:08:38 +0300

    *Modify: Ball, Player and GameManager scripts

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,7 +24,6 @@ public class Player : MonoBehaviour
         Instance = this;
     }
 
-    // Hareket kodlarý için Update yerine FixedUpdate kullanýlabilir.
     void Update()
     {
         CubeMovement();
@@ -33,51 +32,48 @@ public class Player : MonoBehaviour
     #region Cube Movement
     void CubeMovement()
     {
-        if (GameManager.Instance.gameStopped == false)
+        if (!GameManager.Instance.gameStopped && !_isMoving)
         {
-            if (_isMoving == false)
+            //SAÐA SOLA HAREKET
+            if (Input.GetMouseButtonDown(0))
             {
-                //SAÐA SOLA HAREKET
-                if (Input.GetMouseButtonDown(0))
-                {
-                    _mousePosStart = Input.mousePosition;
-                    _posX = this.gameObject.transform.GetChild(0).transform.localPosition.x;
-                }
+                _mousePosStart = Input.mousePosition;
+                _posX = gameObject.transform.GetChild(0).transform.localPosition.x;
+            }
 
-                if (Input.GetMouseButton(0))
-                {
-                    _mousePos = Input.mousePosition;
-                    _sphereX = ((_mousePos.x - _mousePosStart.x) / (Screen.width / 30f)) + _posX;
+            if (Input.GetMouseButton(0))
+            {
+                _mousePos = Input.mousePosition;
+                _sphereX = ((_mousePos.x - _mousePosStart.x) / (Screen.width / 30f)) + _posX;
 
-                    if (_sphereX > 1.5f) _sphereX = 1.5f;
+                if (_sphereX > 1.5f) _sphereX = 1.5f;
 
-                    if (_sphereX < -1.5f) _sphereX = -1.5f;
+                if (_sphereX < -1.5f) _sphereX = -1.5f;
 
-                    this.gameObject.transform.GetChild(0).transform.localPosition = new Vector3(
-                        Mathf.Lerp(this.transform.GetChild(0).transform.localPosition.x, _sphereX, 0.1f),
-                        this.transform.GetChild(0).localPosition.y,
-                        this.transform.GetChild(0).transform.localPosition.z);
-                }
+                gameObject.transform.GetChild(0).transform.localPosition = new Vector3(
+                    Mathf.Lerp(transform.GetChild(0).transform.localPosition.x, _sphereX, 0.1f),
+                    transform.GetChild(0).localPosition.y,
+                    transform.GetChild(0).transform.localPosition.z);
+            }
 
-                if (Input.GetMouseButtonUp(0))
-                {
-                    // Child objeyi al.
-                    GameObject playerBall = this.transform.GetChild(0).gameObject;
-                    Vector3 targetPos = playerBall.transform.localPosition;
-                    targetPos.z = 7.8f;
+            if (Input.GetMouseButtonUp(0))
+            {
+                // Child objeyi al.
+                GameObject playerBall = transform.GetChild(0).gameObject;
+                Vector3 targetPos = playerBall.transform.localPosition;
+                targetPos.z = 7.8f;
 
-                    // Child objeyi force ile ileri taþý.
-                    playerBall.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
-                    playerBall.GetComponent<Rigidbody>().AddForce(0f, 0f, 480f, ForceMode.Impulse);
+                // Child objeyi force ile ileri taþý.
+                playerBall.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+                playerBall.GetComponent<Rigidbody>().AddForce(0f, 0f, 480f, ForceMode.Impulse);
 
-                    playerBall.transform.GetChild(GameManager.Square).gameObject.SetActive(false);
-                    playerBall.transform.GetChild(GameManager.Trail).gameObject.SetActive(true);
+                playerBall.transform.GetChild(GameManager.Square).gameObject.SetActive(false);
+                playerBall.transform.GetChild(GameManager.Trail).gameObject.SetActive(true);
 
-                    // Parentini deðiþ.
-                    playerBall.transform.parent = _enemyCubesParent;
+                // Parentini deðiþ.
+                playerBall.transform.parent = _enemyCubesParent;
 
-                    _isMoving = true;
-                }
+                _isMoving = true;
             }
         }
     }
@@ -86,13 +82,13 @@ public class Player : MonoBehaviour
     #region Create Player Ball
     public void CreatePlayerBall()
     {
-        if (this.transform.childCount < 1)
+        if (transform.childCount < 1)
         {
             // Yeni obje oluþtur.
             string tag = _tags[Random.Range(0, 5)];
 
             GameObject newBall = ObjectPooler.SharedInstance.GetPooledObject(tag);
-            newBall.transform.parent = this.transform;
+            newBall.transform.parent = transform;
             newBall.transform.localPosition = new Vector3(0f, 0.3f, 0f);
             newBall.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
             newBall.transform.GetChild(GameManager.Square).gameObject.SetActive(true);
@@ -100,8 +96,6 @@ public class Player : MonoBehaviour
             newBall.transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
             // newBall.transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
             newBall.SetActive(true);
-
-
 
             _isMoving = false;
         }
